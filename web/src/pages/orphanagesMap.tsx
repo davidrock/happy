14ce69f668b8c399ccea4cfdacb48f89710682ce
David Rock <davidrocha@outlook.com>
2020-10-13 12:51:09 -0300

    create orphages map page

--- web/src/pages/orphanagesMap.tsx
+++ web/src/pages/orphanagesMap.tsx
@@ -1,5 +1,31 @@
 import React from 'react';
+import { FiPlus } from 'react-icons/fi';
+import { Link } from 'react-router-dom';
+import mapMarkerImg from '../images/marker.svg';
+import '../styles/pages/orphanages-map.css';
 
 export default function OrphanagesMap() {
-  return <h1> Ofanatos</h1>;
+  return (
+    <div id="page-map">
+      <aside>
+        <header>
+          <img src={mapMarkerImg} alt="marker" />
+
+          <h2>Escolha um orfanato no mapa</h2>
+          <p>Muitas crianças estão esperando a sua visita :) </p>
+        </header>
+
+        <footer>
+          <strong>Vila Velha</strong>
+          <span>Espírito Santo</span>
+        </footer>
+      </aside>
+
+      <div></div>
+
+      <Link to="" className="create-orphanage">
+        <FiPlus size="32" color="#FFF" />
+      </Link>
+    </div>
+  );
 }
